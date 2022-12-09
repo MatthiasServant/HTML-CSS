1f7e1314a1f4a863e5957866f85b5d30533dc340
Matthias <39270082+MatthiasServant@users.noreply.github.com>
2022-12-09 10:51:34 +0100

    Add files via upload

--- HTMLCSS/main.js
+++ HTMLCSS/main.js
@@ -1,52 +1,86 @@
-const root = document.querySelector('#root');
-const ingredient = document.querySelector('#ingredient');
-
-
-const button = document.createElement('button');
-button.textContent = 'Click me';
-
-root.appendChild(button);
-
-const fetchRandomDrink = async () => {
-    const responseCocktail = await fetch('https://www.thecocktaildb.com/api/json/v1/1/random.php');
-
-    return await responseCocktail.json();
-}
-
-function getIngredients(drink){
-    const ul = document.createElement('ul')
-
-    for(let i = 1; i<=15; i++){
-        if(drink['strIngredient' + i] !== null ){
-        const liEl = document.createElement("li");
-        liEl.textContent = drink['strIngredient' + i];
-        ul.appendChild(liEl);
-        }
+class CocktailFetcher {
+    constructor() {
+      this.root = document.querySelector('#root');
+      this.div = document.querySelector('#div');
     }
+  
+    async getIngredients(cocktail) {
+      const drinkIngredientArray = [];
+  
+      for (let i = 1; i < 16; i += 1) {
+        const ingr = cocktail[`strIngredient${i}`];
+        if (ingr != null) {
+          drinkIngredientArray.push(ingr);
+        }
+      }
+  
+      return drinkIngredientArray;
+    }
+  
+    createElement(type, classes, text = '') {
+      const element = document.createElement(type);
+  
+      for (const className of classes) {
+        element.classList.add(className);
+      }
+  
+      if (text !== '') {
+        element.textContent = text;
+      }
+  
+      this.div.appendChild(element);
+      return element;
+    }
+  
+    async renderSearchedCocktails(name) {
+      try {
+        const response = await fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${name}`);
+        const data = await response.json();
+  
+        const cocktails = data.drinks;
+  
+        if (!cocktails || cocktails.length === 0) {
+          throw new Error('PAS DE COCKTAIL QUI PORTE CE NOM :)');
+        }
+  
+        for (const cocktail of cocktails) {
+          const drinkThumbnail = this.createElement('img', '', cocktail.strDrinkThumb);
+          drinkThumbnail.src = cocktail.strDrinkThumb;
+          const drinkName = this.createElement('h1', '', cocktail.strDrink);
+          const drinkInstruction = this.createElement('span', '', cocktail.strInstructions);
+          const drinkCategory = this.createElement('h2', '', cocktail.strCategory);
+          const drinkIngredients = this.createElement('span', '', await this.getIngredients(cocktail));
+  
+          this.root.append(drinkName);
+          this.root.append(drinkThumbnail);
+          this.root.append(drinkCategory);
+          this.root.append(drinkInstruction);
+          this.root.append(drinkIngredients);
+        }
+      } catch (error) {
+        throw new Error(error.message);
+      }
+    }
+  
+    clear() {
+      this.root.innerHTML = null;
+    }
+  }
+  
+  const cocktailFetcher = new CocktailFetcher();
+  
+  const button = cocktailFetcher.createElement('button', '', 'Liste de cocktails');
+  const searchInput = cocktailFetcher.createElement('input', '', '');
+
+  
+  button.addEventListener('click', async () => {
+      cocktailFetcher.clear();
+      cocktailFetcher.renderSearchedCocktails();
+  });
     
-    return ul;
-}
-
-button.addEventListener("click", async () =>{
-   ingredient.innerHTML = '';
-   const response = await fetchRandomDrink();
-   const randomDrink = response.drinks[0];
-   console.log(randomDrink);
-
-   const h1 = document.createElement('h1');
-   const h2 = document.createElement('h3');
-   const img = document.createElement('img');
-   const p = document .createElement('p');
-
-   h1.textContent = randomDrink.strDrink;
-   h2.textContent = randomDrink.strCategory;
-   img.src = randomDrink.strDrinkThumb;
-   p.textContent = randomDrink.strInstructions;
-
-   ingredient.appendChild(h1);
-   ingredient.appendChild(h2);
-   ingredient.appendChild(img);
-   ingredient.appendChild(getIngredients(randomDrink));
-   ingredient.appendChild(p);
-
-});
+  searchInput.addEventListener('change', async (event) => {
+      const searchTerm = event.target.value;
+    
+      cocktailFetcher.clear();
+      cocktailFetcher.renderSearchedCocktails(searchTerm);
+  });
