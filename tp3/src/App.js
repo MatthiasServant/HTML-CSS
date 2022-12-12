7a31bf897018d7006c141a390de28ed2752311c7
Matthias <39270082+MatthiasServant@users.noreply.github.com>
2022-12-12 16:25:32 +0100

    Add files via upload

---
+++ tp3/src/App.js
@@ -0,0 +1,15 @@
+import Footer from "./Footer"
+import Header from "./Header"
+import RandomCocktail from "./RandomCocktail";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <RandomCocktail />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ tp3/src/Footer.js
@@ -0,0 +1,9 @@
+const Foooter = () => {
+    return(
+        <footer>
+            <p> Footer </p>
+        </footer>
+    )
+}
+
+export default Foooter;
