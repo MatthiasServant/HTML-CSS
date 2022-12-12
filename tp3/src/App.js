a8c17f86a4f6c7df8c7a391f98307c06f5fbd9af
Matthias <39270082+MatthiasServant@users.noreply.github.com>
2022-12-12 14:32:14 +0100

    Add files via upload

---
+++ tp3/src/App.js
@@ -0,0 +1,15 @@
+import Footer from "./Footer"
+import Header from "./Header"
+import ListArticles from "./ListArticles";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <ListArticles />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
